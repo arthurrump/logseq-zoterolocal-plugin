3229c03a009ae2cb71068b33a5d824978673705a
Arthur Rump <hello@arthurrump.com>
2025-08-13 15:33:10 +0200

    Add support for linked_file linkMode and built-in style Zotero links

--- src/settings.ts
+++ src/settings.ts
@@ -69,6 +69,20 @@ export const handleSettings = async ({
           'Specify how authors should be shown in the properties. Available placeholders: <% firstName %>, <% lastName %>, <% creatorType %>',
         default: '<% firstName %> <% lastName %> (<% creatorType %>)',
       },
+      {
+        key: 'useZoteroLinks',
+        type: 'boolean',
+        title: 'Use Zotero Links',
+        description: 'Use zotero:// links for attachments, instead of local file links',
+        default: false
+      },
+      {
+        key: 'linkedAttachmentBasePath',
+        type: 'string',
+        title: 'Linked Attachment Base Directory',
+        description: `Base directory for linked attachments. Should be the same value as the option in Zotero (under Settings > Advanced). This is not used if the 'Use Zotero Links' option is enabled`,
+        default: ''
+      }
     ]
 
     settings = [...settings, ...pluginSettings]
